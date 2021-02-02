8be5ae2ff716ddcc9bff789884acd8f469651c56
adrshojk <adrshojk@gmail.com>
2021-02-02 08:35:46 +0530

    redux dev tools for debugging

--- UI/adrshojk/src/redux/store.js
+++ UI/adrshojk/src/redux/store.js
@@ -6,5 +6,6 @@ const rootReducer = combineReducers({
     userInfo: userReducer,
     surveyData: surveyReducer
 })
+//REDUX DEV TOOLS for DEBUGGING
 const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store
